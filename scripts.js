d662ab40065d3bf5c789f48393befd6b98be0a4c
Lachicagradiadora <200379@unsaac.edu.pe>
2023-05-02 18:04:04 -0500

    html well explained

---
+++ scripts.js
@@ -0,0 +1,6 @@
+document.querySelector('h1').addEventListener('click', ()=>{ 
+    document.body.style.background='blue';
+})
+document.querySelectorAll.addEventListener('scroll', ()=>{
+    document.head.style.backgroundColor='yellow'
+})
