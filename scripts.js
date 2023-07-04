3d7893d3756b354c09f2dbfb01bfa32b88b8ead5
Lachicagladiadora <200379@unsaac.edu.pe>
2023-07-03 22:33:39 -0500

    change pges links

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 document.querySelector('h1').addEventListener('click', ()=>{ 
-    document.body.style.background='blue';
+    document.body.style.background='orange';
 })
 document.querySelectorAll.addEventListener('scroll', ()=>{
-    document.head.style.backgroundColor='yellow'
+    document.head.style.background='yellow'
 })
